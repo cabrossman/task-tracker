1628d2bf670be484b9fa738c6f01433fc94dae26
Christopher Brossman <cabrossman1@gmail.com>
2026-09-12 12:43:14 -0700

    Add more example tasks

--- tasks.py
+++ tasks.py
@@ -21,6 +21,11 @@ def show_tasks():
 add_task("Buy groceries")
 add_task("Go to gym")
 add_task("Study ML")
+add_task("Read a chapter of a book")
+add_task("Do laundry")
+add_task("Water the plants")
+add_task("Plan meals for the week")
+add_task("Practice Python")
 
 complete_task("Go to gym")
 
